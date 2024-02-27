60b2c61312f29339a14827d81c5e6213990db8ab
ayan-joshi <ayanjoshi182003@gmail.com>
2024-02-27 22:23:00 +0530

    commit

--- src/app/components/MiddleSection/Middle.jsx
+++ src/app/components/MiddleSection/Middle.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Middle() {
-  return <div className="h-screen ">Middle</div>;
+  return ;
 }
 
 export default Middle;
--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Landing from "./Landingpage/page";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between ">
+      
       <Landing />
     </main>
   );
